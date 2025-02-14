bb991bfe325e15ce7c86cc610584b43eed27c13b
SjSlayde <xaizeraven@gmail.com>
2025-02-14 11:56:14 +0100

    index en cours

--- VillageGreen/src/Controller/CatalogueController.php
+++ VillageGreen/src/Controller/CatalogueController.php
@@ -5,14 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RubriqueRepository;
+use App\Repository\SousRubriqueRepository;
+use App\Repository\ProduitRepository;
 
-final class CatalogueController extends AbstractController
+class CatalogueController extends AbstractController
 {
+
+    private $rubriqueRepo;
+    private $produitRepo;
+    private $SousRubriqueRepo;
+
+    public function __construct(RubriqueRepository $rubriqueRepo,ProduitRepository $produitRepo,SousRubriqueRepository $SousRubriqueRepo)
+    {
+        $this->rubriqueRepo = $rubriqueRepo;
+        $this->produitRepo = $produitRepo;
+        $this->SousRubriqueRepo = $SousRubriqueRepo;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $rubriques = $this->rubriqueRepo->findAll();
+        // $sousRubriques = $this->produitRepo->all();
+        $produits = $this->produitRepo->findAll();
+
         return $this->render('catalogue/index.html.twig', [
             'controller_name' => 'CatalogueController',
+            'rubriques' => $rubriques,
+            'produits' => $produits
         ]);
     }
 }
