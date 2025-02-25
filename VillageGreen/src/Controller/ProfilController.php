42f0ae992a6760844e225ec8cefb494deb5828f1
SjSlayde <xaizeraven@gmail.com>
2025-02-25 15:33:15 +0100

    profil en cour

--- VillageGreen/src/Controller/ProfilController.php
+++ VillageGreen/src/Controller/ProfilController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\AdresseRepository;
+use App\Repository\AffiliationAdresseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
-    {
-        return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
-        ]);
+    private $adresserepo;
+    private $affiliationAdressesrepo;
+
+    public function __construct(AdresseRepository $adresserepo,AffiliationAdresseRepository $affiliationAdresseRepo){
+        $this->adresserepo = $adresserepo;
+        $this->affiliationAdressesrepo = $affiliationAdresseRepo;
     }
+    #[Route('/profil{nomuser}_{prenomuser}', name: 'app_profil')]
+    public function profil(): Response
+    {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        /** @var \App\Entity\Utilisateur $user */
+        $user = $this->getUser();
+        $affilLivs = $this->affiliationAdressesrepo->findBy( ['client' => $user, 'type' => 'adLivraison']);
+        
+        $adressesLiv = [];
+
+        foreach ($affilLivs  as $adresseLiv) {
+            array_push($adressesLiv,$adresseLiv->getAdresse());
+        }
+
+        return $this->render('profil/index.html.twig', [
+            'user' => $user,
+            'adresses' => $adressesLiv
+        ]);
+}
 }
