9d74811eafdb4641463b6ca984e495233b711289
SjSlayde <xaizeraven@gmail.com>
2025-04-29 09:12:58 +0200

    test unitaire

--- VillageGreen/tests/Controller/PostControllerTest.php
+++ VillageGreen/tests/Controller/PostControllerTest.php
@@ -13,5 +13,45 @@ class PostControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Les Rubriques :');
+
+        $tab = $crawler->filter('h5');
+        $this->assertEquals($tab->count(), 11);
+    }
+
+    public function test2(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/sous-rubrique');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Les Sous-Rubriques :');
+
+        $tab = $crawler->filter('h5');
+        $this->assertEquals($tab->count(), 12);
+    }
+
+    public function test3(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/produits');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Nos produits :');
+
+        $tab = $crawler->filter('h5');
+        $this->assertEquals($tab->count(), 20);
+    }
+
+    public function test4(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/produits-2');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Nos produits :');
+        $this->assertSelectorTextContains('h5', 'VIOLON');
+
+        $tab = $crawler->filter('h5');
+        $this->assertEquals($tab->count(), 2);
     }
 }
